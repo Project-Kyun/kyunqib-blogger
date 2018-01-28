18c9900620f93faeb52e793e09a472196419e572
Project-Kyun <muhammadaqibnurmanmuftinew@gmail.com>
2018-01-29 06:25:23 +0700

    Update and rename js-post-animeloker.js to js-post-animeloker-1.js

---
+++ js-post-animeloker-1.js
@@ -0,0 +1,15 @@
+//<![CDATA[
+//*This File From KyunQIB AnimeLoker Website*//
+var myApp=angular.module('tabs',[]);
+myApp.controller('shift_tabs',function(){
+  this.activeTab;
+  this.makeShift=function(e){
+    this.activeTab=e;
+  }
+  this.isActive=function(f){
+    if(f==this.activeTab){
+      return true
+    }
+  }
+});
+//]]>
