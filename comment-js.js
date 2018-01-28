d208e6f9f9018f39e5452be7b1a72f01c438d0f3
Project-Kyun <muhammadaqibnurmanmuftinew@gmail.com>
2018-01-28 23:30:56 +0700

    Update comment-js.js

--- comment-js.js
+++ comment-js.js
@@ -1,13 +1,3 @@
-    <b:if cond='data:post.numComments != 0'>
-    var Items = <data:post.commentJso/>;
-    var Msgs = <data:post.commentMsgs/>;
-    var Config = <data:post.commentConfig/>;
-    
-    <b:else/>
-    var Items = {};
-    var Msgs = {};
-    var Config = {&#39;maxThreadDepth&#39;:&#39;0&#39;};
-    </b:if>
     //<![CDATA[
     //Global setting
     Config.maxThreadDepth = 3;
