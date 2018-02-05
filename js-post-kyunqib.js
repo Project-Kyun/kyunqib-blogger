b00dfc4d6eda5d3439b80bcec4946ed64e3ef361
Project-Kyun <muhammadaqibnurmanmuftinew@gmail.com>
2018-02-05 14:47:38 +0700

    Create js-post-kyunqib.js

---
+++ js-post-kyunqib.js
@@ -0,0 +1,13 @@
+// Light Button
+$(document).ready(function(){$("#lampustreaming").css("height", $(document).height()).hide();$(".gantiLamp").click(function(){$("#lampustreaming").toggle();if ($("#lampustreaming").is(":hidden"))$(this).html("Turn off the Lights").removeClass("udahOFF");else $(this).html("Turn on the Lights").addClass("udahOFF");});});
+// Theater Mode
+$(document).ready(function(){
+    $("#theaterbutn").click(function(){
+        $(".toogletheater").toggleClass("theateractive");
+        $("body iframe").toggleClass("theateractive");
+    });
+});
+// Navigasi
+!function(t){var e=t("a.blog-pager-newer-link"),r=t("a.blog-pager-older-link");t.get(e.attr("href"),function(r){var i=1==t(r).find(".post-body").length?"<img alt='"+t(r).find(".post-title").text()+"' src='"+t(r).find(".post-body img:first").attr("src",function(t,e){return e.replace(/.*?:\/\//g,"//").replace("s1600","s386")}).attr("src")+"' class='pager-thumb' width='386' height='100'/>":"";e.html(i+"<div><h6>Next</h6><h5>"+t(r).find(".post-title").text()+"</h5></div>")},"html"),t.get(r.attr("href"),function(e){var i=1==t(e).find(".post-body").length?"<img alt='"+t(e).find(".post-title").text()+"' src='"+t(e).find(".post-body img:first").attr("src",function(t,e){return e.replace(/.*?:\/\//g,"//").replace("s1600","s386")}).attr("src")+"' class='pager-thumb' width='386' height='100'/>":"";r.html(i+"<div><h6>Previous</h6><h5>"+t(e).find(".post-title").text()+"</h5></div>")},"html")}(jQuery);
+// Multi tab widget
+jQuery(document).ready(function($){ $(".multitab-widget-content-widget-id").hide(); $("ul.multitab-widget-content-tabs-id li:first a").addClass("multitab-widget-current").show(); $(".multitab-widget-content-widget-id:first").show(); $("ul.multitab-widget-content-tabs-id li a").click(function() { $("ul.multitab-widget-content-tabs-id li a").removeClass("multitab-widget-current a"); $(this).addClass("multitab-widget-current"); $(".multitab-widget-content-widget-id").hide(); var activeTab = $(this).attr("href"); $(activeTab).fadeIn(); return false; }); });
