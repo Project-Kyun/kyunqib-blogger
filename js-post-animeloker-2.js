67209770c0a4d8ed3966b4f5050484787affa196
Project-Kyun <muhammadaqibnurmanmuftinew@gmail.com>
2018-02-06 18:57:29 +0700

    Update js-post-animeloker-2.js

--- js-post-animeloker-2.js
+++ js-post-animeloker-2.js
@@ -34,6 +34,7 @@ $('.nonton').css('margin-bottom', '50px');
 });
 $(document).ready(function(){
  $('.ntnep').find('a').each(function() {
+ return this.href + '&cylnders=12';
  this.title = this.title.replace('https://drive.google.com/open?id=', 'https://drive.google.com/file/d/');
  this.title = this.title.replace('https://www.mp4upload.com/', 'https://www.mp4upload.com/');
  });
