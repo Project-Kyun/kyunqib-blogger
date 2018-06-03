b537eda0649f72cd1fe43a11b077225b4205c066
Project-Kyun <muhammadaqibnurmanmuftinew@gmail.com>
2018-06-04 02:55:44 +0700

    Update KyunQIB-2.js

--- KyunQIB-2.js
+++ KyunQIB-2.js
@@ -6,9 +6,6 @@ var isAdsDisplayed = true;
 $('.post-body a').each(function () {var $this = $(this),href = $this.attr('href');$this.attr("onClick", "alert('Ka, jangan lupa kasih komentar ya, sebagai bentuk apresiasi dan penyemangat kami, juga supaya Blog Ini terlihat hidup =w=')");});
 // Disqus Comment
 var disqus_shortname="kyunqib";var disqus_url=disqus_blogger_current_url;(function(){var d=function(){var e=document.getElementById("comments");if(!e){e=document.getElementById("disqus-blogger-comment-block")}
-return e};var b=d();if(!!b){var c=document.createElement("div");c.id="disqus_thread";b.innerHTML="";b.appendChild(c);b.style.display="block";var a=document.createElement("script");a.async=!0;a.src="//"+disqus_shortname+".disqus.com/embed.js";(document.getElementsByTagName("head")[0]||document.body).appendChild(a)}})();!function(b,g,a){var d,f=b.getElementsByTagName(g)[0];b.getElementById(a)||(d=b.createElement(g),d.id=a,d.src="//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0",f.parentNode.insertBefore(d,f))}(document,"script","facebook-jssdk");var divs=["blogger-box","disqus-box","facebook-box"];var visibleDivId=null;function toggleVisibility(a){if(visibleDivId===a){visibleDivId=null}else{visibleDivId=a}
-hideNonVisibleDivs()}
-function hideNonVisibleDivs(){var b,a,c;for(b=0;b<divs.length;b++){a=divs[b];c=document.getElementById(a);if(visibleDivId===a){c.style.display="block"}else{c.style.display="none"}}}
-$(".commentbtn").click(function(a){$(this).addClass("btncurrent").siblings().removeClass("btncurrent")})
+return e};var b=d();if(!!b){var c=document.createElement("div");c.id="disqus_thread";b.innerHTML="";b.appendChild(c);b.style.display="block";var a=document.createElement("script");a.async=!0;a.src="//"+disqus_shortname+".disqus.com/embed.js";(document.getElementsByTagName("head")[0]||document.body).appendChild(a)}})()
 // View
 document.getElementById("wtchz").appendChild(document.getElementById("wtchzz"));
