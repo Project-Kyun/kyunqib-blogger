b2899fd431890b72aa1c757ded95509b8cacbf4d
Project-Kyun <muhammadaqibnurmanmuftinew@gmail.com>
2018-06-06 08:52:22 +0700

    Update KyunQIB-2.js

--- KyunQIB-2.js
+++ KyunQIB-2.js
@@ -5,7 +5,4 @@ var isAdsDisplayed = true;
 // Onclick Notif
 $('.post-body a').each(function () {var $this = $(this),href = $this.attr('href');$this.attr("onClick", "alert('Ka, jangan lupa kasih komentar ya, sebagai bentuk apresiasi dan penyemangat kami, juga supaya Blog Ini terlihat hidup =w=')");});
 // Disqus Comment
-var disqus_shortname="kyunqib";var disqus_url=disqus_blogger_current_url;(function(){var d=function(){var e=document.getElementById("comments");if(!e){e=document.getElementById("disqus-blogger-comment-block")}
-return e};var b=d();if(!!b){var c=document.createElement("div");c.id="disqus_thread";b.innerHTML="";b.appendChild(c);b.style.display="block";var a=document.createElement("script");a.async=!0;a.src="//"+disqus_shortname+".disqus.com/embed.js";(document.getElementsByTagName("head")[0]||document.body).appendChild(a)}})()
-// View
-document.getElementById("wtchz").appendChild(document.getElementById("wtchzz"));
+function load_Comments(){var e=document.getElementById("disqus-kyun");e.style.display="inline-block";var t="kyunqib";!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://"+t+".disqus.com/embed.js",(document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(e)}()}var disqus_shortname="kyunqib";!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="//"+disqus_shortname+".disqus.com/blogger_item.js",(document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(e)}();
